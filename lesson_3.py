9a94c67cffd444cdf49e7e65b3fafc2a550dbfdd
Vitaly <symbol92balaklava@gmail.com>
2024-09-26 19:14:17 +0300

    Frankenstein

--- lesson_3.py
+++ lesson_3.py
@@ -1,51 +1,61 @@
-#Проверяет правильность ввода имени
-def print_text(text: str):
-    print(text)
+# Проверяет правильность ввода имени
 
-def chech_name() -> str:
+def check_name(name: str) -> tuple:
+    text = ''
+    if not name.isalpha():
+        text = 'the name must consist of letters'
 
-    while True:
-        name_input = input('enter your name: ')
-        if not name_input.isalpha():
-            print_text('the name must consist of letters')
+    elif len(name) <= 3:
+        text = 'longer than 3 characters'
 
-        elif len(name_input) <= 3:
-            print_text('longer than 3 characters')
+    return (text, name)
 
+
+# Проверяет правильности ввода возраста
+def check_age(age: str) -> tuple:
+    text = ''
+    if not age.isdigit():
+        text = 'You need to enter a number: '
+
+    elif int(age) == 0:
+        text = 'age cannot be 0'
+
+    elif int(age) < 14:
+        text = 'age cannot be less than 14'
+
+    return (text, age)
+
+
+def print_name_age():
+    list_func = [check_name, check_age]
+    name_age = []
+    count = 0
+
+    while len(name_age) >= count:
+        # условие, при котором запустим все check функций
+
+        if len(name_age) < len(list_func):
+            input_name = ('name', 'age')
+            text = ''
+            func = list_func[count](input(f'Input {input_name[count]}: '))
+            if func[0]:
+                text = func[0]
+            else:
+                name_age.append(func[1])
+                count += 1
         else:
-            break
+            count += 1
+            name = name_age[0].capitalize()
+            age = name_age[1]
 
-    return name_input
+            text = f'Hello {name}. You are {age} years old.'
 
-#Проверяет правильности ввода возраста
-def check_age() -> int:
+            if 16 <= int(age) <= 17:
+                text = f"{text} Don't forget to get your first passport."
+            elif 25 <= int(age) <= 26 or 45 <= int(age) <= 46:
+                text = f"{text} don't forget to replace your passport."
 
-    while True:
-        age_input = input('enter your age: ')
-
-        if not age_input.isdigit():
-            print_text('You need to enter a number: ')
-        elif int(age_input) == 0:
-            print_text('age cannot be 0')
-
-        elif int(age_input) < 14:
-            print_text('age cannot be less than 14')
-
-        else:
-            break
-
-    return int(age_input)
+        print(text)
 
 
-name = chech_name().capitalize()
-age = check_age()
-
-hello_print = f'Hello {name}. You are {age} years old.'
-
-if 16 <= age <= 17:
-    hello_print = f"{hello_print} Don't forget to get your first passport."
-elif 25 <= age <= 26 or 45 <= age <= 46:
-    hello_print = f"{hello_print} don't forget to replace your passport."
-print_text(hello_print)
-
-
+print_name_age()
