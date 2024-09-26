526e62c8d4561ed83564f5c859cd44e442aa2191
Vitaly <symbol92balaklava@gmail.com>
2024-09-26 15:18:12 +0300

    add def print_text

--- lesson_3.py
+++ lesson_3.py
@@ -1,15 +1,16 @@
 #Проверяет правильность ввода имени
+def print_text(text: str):
+    print(text)
+
 def chech_name() -> str:
-    def print_error_name(error_name: str):
-        print(error_name)
 
     while True:
         name_input = input('enter your name: ')
         if not name_input.isalpha():
-            print_error_name('the name must consist of letters')
+            print_text('the name must consist of letters')
 
         elif len(name_input) <= 3:
-            print_error_name('longer than 3 characters')
+            print_text('longer than 3 characters')
 
         else:
             break
@@ -18,19 +19,17 @@ def chech_name() -> str:
 
 #Проверяет правильности ввода возраста
 def check_age() -> int:
-    def print_error_age(error_age: str):
-        print(error_age)
 
     while True:
         age_input = input('enter your age: ')
 
         if not age_input.isdigit():
-            print_error_age('You need to enter a number: ')
+            print_text('You need to enter a number: ')
         elif int(age_input) == 0:
-            print_error_age('age cannot be 0')
+            print_text('age cannot be 0')
 
         elif int(age_input) < 14:
-            print_error_age('age cannot be less than 14')
+            print_text('age cannot be less than 14')
 
         else:
             break
@@ -40,11 +39,13 @@ def check_age() -> int:
 
 name = chech_name().capitalize()
 age = check_age()
+
 hello_print = f'Hello {name}. You are {age} years old.'
+
 if 16 <= age <= 17:
     hello_print = f"{hello_print} Don't forget to get your first passport."
 elif 25 <= age <= 26 or 45 <= age <= 46:
     hello_print = f"{hello_print} don't forget to replace your passport."
-print(hello_print)
+print_text(hello_print)
 
 
