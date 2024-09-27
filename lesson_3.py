c4cae536b122e5cf3e7496b6e7e807935f19f0f6
Vitaly <symbol92balaklava@gmail.com>
2024-09-27 13:34:46 +0300

    remake

--- lesson_3.py
+++ lesson_3.py
@@ -1,5 +1,4 @@
-
-def check_name(name: str) -> tuple:
+def check_name(name: str) -> str:
     text = ''
     if not name.isalpha():
         text = 'the name must consist of letters'
@@ -7,9 +6,10 @@ def check_name(name: str) -> tuple:
     elif len(name) <= 3:
         text = 'longer than 3 characters'
 
-    return (text, name)
+    return text
 
-def check_age(age: str) -> tuple:
+
+def check_age(age: str) -> str:
     text = ''
     if not age.isdigit():
         text = 'You need to enter a number: '
@@ -20,34 +20,42 @@ def check_age(age: str) -> tuple:
     elif int(age) < 14:
         text = 'age cannot be less than 14'
 
-    return (text, age)
+    return text
 
 
 def print_name_age():
-    list_func = (check_name, check_age)
-    name_age = []
+    name = 'name'
+    age = 'age'
     count = 0
-    input_name = ('name', 'age')
+    flag = True
 
-    while count < 3:
+    while flag:
 
         if count < 2:
-            user_input = input(f'Input {input_name[count]}: ')
-            text, parameter = list_func[count](user_input)
+            if count == 0:
+                user_input = input(f'Input {name}: ')
+                text = check_name(user_input)
+            elif count == 1:
+                user_input = input(f'Input {age}: ')
+                text = check_age(user_input)
 
             if not text:
-                name_age.append(parameter)
+
+                if count == 0:
+                    name = user_input
+                elif count == 1:
+                    age = user_input
+
                 count += 1
+                continue
 
         else:
-            name = name_age[0].capitalize()
-            age = name_age[1]
-            text = f'Hello {name}. You are {age} years old.'
+            text = f'Hello {name.capitalize()}. You are {age} years old.'
             if 16 <= int(age) <= 17:
                 text = f"{text} Don't forget to get your first passport."
             elif 25 <= int(age) <= 26 or 45 <= int(age) <= 46:
                 text = f"{text} don't forget to replace your passport."
-            count += 1
+            flag = False
 
         print(text)
 
